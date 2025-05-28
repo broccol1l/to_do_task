855fe6349130de7ff9f8fe8006bc8525ced3cc12
broccol1l <xamrakulovramazan@gmail.com>
2025-05-28 14:04:21 +0500

    upd-http-status

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 from database.task_service import add_task, get_all_tasks, get_task_by_id, update_task, delete_task
 from typing import Optional
@@ -32,13 +32,23 @@ async def get_all_tasks_api():
 
 @app.get("/tasks/{task_id}", summary="Get task by id", description="Enter id and get exact task")
 async def get_task_by_id_api(task_id: int):
-    return await get_task_by_id(task_id)
+    task = await get_task_by_id(task_id)
+    if not task:
+        raise HTTPException(status_code=404, detail=f"Task with id {task_id} not found")
+    return task
 
 @app.put("/tasks/{task_id}", summary="Update task", description="Update every or exact field if needed")
 async def update_task_api(task_id: int, new_title: str | None = None, new_description: str | None = None,
                       is_completed_upd: bool | None = None):
-    return await update_task(task_id, new_title, new_description, is_completed_upd)
+    updated_task = await update_task(task_id, new_title, new_description, is_completed_upd)
+    if not updated_task:
+        raise HTTPException(status_code=404, detail=f"Task with id {task_id} not found")
+    return updated_task
+
 
 @app.delete("/tasks/{task_id}", summary="Delete task", description="Just delete task by id")
 async def delete_task_api(task_id: int):
-    return await delete_task(task_id)
+    deleted_task = await delete_task(task_id)
+    if not deleted_task:
+        raise HTTPException(status_code=404, detail=f"Task with id {task_id} not found")
+    return {"detail": "Task deleted successfully"}
